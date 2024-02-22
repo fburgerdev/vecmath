9ce092585773ba6aeeda14c1254fdfd46bd2640c
Florian Burger <florianburger03@gmail.com>
2024-02-22 14:25:47 +0100

    Updated Example

--- example/main.cpp
+++ example/main.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include "to_string.hpp"
+#include "tostring.hpp"
+#include "matrixconstruct.hpp"
+#include "matrixarithmetic.hpp"
+#include "matrixalias.hpp"
+#include "vectoralias.hpp"
+#include "matrixfunc.hpp"
+#include "vectorfunc.hpp"
 
 using namespace Math;
-template<address N, address M>
-void dosth(int (&&arr)[N][M]) {
-    std::cout << N << std::endl;
-}
 int main() {
-    Vec4f vec(1, 2, 3, 4);
     Mat4f mat({ 
         1, 2, 0, 0,
-        0, 1, 0, 0,
+        0, 1, 3, 0,
         0, 0, 1, 0,
         0, 0, 0, 1
     });
-    std::cout << "Hello World!" << std::endl;
     std::cout << transposed(mat) * mat<< std::endl;
+    Vec4f vec(1, 2, 3, 4);
     std::cout << dot(vec) << std::endl;
     return EXIT_SUCCESS;
 }
